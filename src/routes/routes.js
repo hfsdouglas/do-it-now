ae21aa3027b98473ec4353dae176637d1b41cb36
Douglas Faria <hfsdouglas@gmail.com>
2024-09-27 21:35:54 -0300

    feat: add routes file pre build

---
+++ src/routes/routes.js
@@ -0,0 +1,37 @@
+export const routes = [
+    {
+        method: 'GET', 
+        path: buildRoutePath('/tasks'), 
+        handler: (req, res) => {
+            
+        }
+    },
+    {
+        method: 'POST', 
+        path: buildRoutePath('/tasks'), 
+        handler: (req, res) => {
+            
+        }
+    },
+    {
+        method: 'PUT', 
+        path: buildRoutePath('/tasks'), 
+        handler: (req, res) => {
+            
+        }
+    },
+    {
+        method: 'DELETE', 
+        path: buildRoutePath('/tasks'), 
+        handler: (req, res) => {
+            
+        }
+    },
+    {
+        method: 'PATCH', 
+        path: buildRoutePath('/tasks'), 
+        handler: (req, res) => {
+            
+        }
+    },
+]
